3bae13973899dd4c834751462416ff23744bc13b
Tao <taovolle.tv@gmail.com>
2026-09-18 09:34:01 +0000

    Exercice 1.8

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,55 @@
+#include <stdio.h>
 
+int main() {
+    int num1 = 16;
+    int num2 = 3;
+    char op = '+';
+
+    switch (op) {
+        case '+':
+            printf("Resultat : %d\n", num1 + num2);
+            break;
+
+        case '-':
+            printf("Resultat : %d\n", num1 - num2);
+            break;
+
+        case '*':
+            printf("Resultat : %d\n", num1 * num2);
+            break;
+
+        case '/':
+            if (num2 != 0) {
+                printf("Resultat : %d\n", num1 / num2);
+            } else {
+                printf("Erreur : division par zero\n");
+            }
+            break;
+
+        case '%':
+            if (num2 != 0) {
+                printf("Resultat : %d\n", num1 % num2);
+            } else {
+                printf("Erreur : modulo par zero\n");
+            }
+            break;
+
+        case '&':
+            printf("Resultat : %d\n", num1 & num2);
+            break;
+
+        case '|':
+            printf("Resultat : %d\n", num1 | num2);
+            break;
+
+        case '~':
+            printf("Resultat : %d\n", ~num1);
+            break;
+
+        default:
+            printf("Operateur invalide\n");
+            break;
+    }
+
+    return 0;
+}
