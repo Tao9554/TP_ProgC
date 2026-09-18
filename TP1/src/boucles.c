b59af0ebdc6386e66b72c6c628b2cbf5a95049d6
Tao <taovolle.tv@gmail.com>
2026-09-18 09:29:22 +0000

    Exercice 1.6 avec while

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -2,12 +2,18 @@
 
 int main() {
     int compteur = 5;
+    int i = 1;
 
-    for (int i = 1; i <= compteur; i++) {
-        for (int j = 1; j <= i; j++) {
+    while (i <= compteur) {
+        int j = 1;
+
+        while (j <= i) {
             printf("* ");
+            j++;
         }
+
         printf("\n");
+        i++;
     }
 
     return 0;
