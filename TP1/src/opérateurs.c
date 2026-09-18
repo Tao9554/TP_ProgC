f24092565f54431f4d219769e630c8d71b0f33a5
Tao <taovolle.tv@gmail.com>
2026-09-18 09:20:38 +0000

    Exercice 1.5

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,17 @@
+#include <stdio.h>
 
+int main() {
+    int a = 16;
+    int b = 3;
 
+    printf("Addition : %d\n", a + b);
+    printf("Soustraction : %d\n", a - b);
+    printf("Multiplication : %d\n", a * b);
+    printf("Division : %d\n", a / b);
+    printf("Modulo : %d\n", a % b);
+
+    printf("a est egal a b : %d\n", a == b);
+    printf("a est superieur a b : %d\n", a > b);
+
+    return 0;
+}
